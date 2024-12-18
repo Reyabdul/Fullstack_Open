202caaa58a337cb97ca7b93edaf51dbbe11f4387
Reyabdul <reyabdul@gmail.com>
2024-12-18 13:19:20 +0900

    Create 'Header' and 'Content' component. Both displays their respected data.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{props.course}</h1>
     </>
+  );
+};
+
+const Part = (props) => {
+  
+
+  
+
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+
   )
 }
 
-export default App
+const Content = () => {
+  const parts = [
+    { part: "Fundamentals of React", exercise: 10 },
+    { part: "Using props to pass data", exercise: 7 },
+    { part: "State of a component", exercise: 15 },
+  ]
+  return (
+    <div>
+      <Part part={`${parts[0].part} ${parts[0].exercise}`}/>
+      <Part part={`${parts[1].part} ${parts[1].exercise}`}/>
+      <Part part={`${parts[2].part} ${parts[2].exercise}`}/>
+    </div>
+  );
+};
+
+function App() {
+  
+  //const-definitions
+  const course = "Half Stack application development";
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
